1a1b36ed397ffc1a336eaedfd0362182b2c31fbc
Ubuntu <ubuntu@volume.cloud.compas.cs.stonybrook.edu>
2018-04-09 20:57:39 +0000

    transfer

---
+++ server.js
@@ -0,0 +1,61 @@
+var express = require("express");
+var app = express();
+var mc = require('mc');
+var client = new mc.Client();
+client.connect(()=>{console.log("connected to memcache on local host port 11211!")});
+client.setAdapter(mc.Adapter.json)
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'jastworld',
+  password : 'jastworld',
+  database : 'hw7'
+});
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+connection.connect();
+
+app.get("/hw7",(req,res,next)=>{
+	///hw7?club=HOU&pos=M
+	var club = req.query.club+"";
+	var position = req.query.pos+"";
+	console.log(position+" <> "+club)
+	client.get(club.toUpperCase()+position.toUpperCase(), function(err,response){
+		if(!err){
+			return response[club.toUpperCase()+position.toUpperCase()];	
+	
+		}else{
+
+		}
+	});
+	//{ club:, pos:, max_assists:, player:, avg_assists:}
+			connection.query('select * from `assists` where `POS` = ? and `club` = ? ORDER BY A DESC, GS DESC',[position,club], function(err,player){
+		//console.log(err)
+		//console.log(player)
+				if(err)
+		   			 return res.json({status: "OK"});
+		//console.log(req.query.pos);
+		//console.log(req.query.club);
+				connection.query('SELECT AVG(A) AS average FROM assists',function(err,result){
+			
+			
+				return res.json({
+					club : player[0].Club,
+					pos : player[0].POS,
+					max_assists: player[0].A,
+					player : player[0].Player,
+					avg_assists: result[0].average
+					});
+				});
+			});
+
+		});
+
+var port = process.env.port || 80;
+app.listen(port,()=>{
+	console.log("App is running on port:"+ port);
+})
+
